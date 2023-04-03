745490b196af3094fb9105e30c48e18c70bd1e3d
Long Ho <long_ho@datahouse.com>
2023-04-03 08:02:46 +0700

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '@src/environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase.config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  name: 'default',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyBvxxMfHK4fXXSXVZiDv13n5QmKFu--YvI',
+      authDomain: 'courseapp-c3885.firebaseapp.com',
+      projectId: 'courseapp-c3885',
+      storageBucket: 'courseapp-c3885.appspot.com',
+      messagingSenderId: '413399314169',
+      appId: '1:413399314169:web:fdb659aa37297c1f88fc28',
+    },
+  },
+};
