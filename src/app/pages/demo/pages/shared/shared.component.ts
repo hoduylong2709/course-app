6b9d25379c6a6e7d37bfa800c3e5abf6a54edcdf
Long Ho <long_ho@datahouse.com>
2023-04-05 17:15:22 +0700

    4th commit

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { regex, regexErrors } from '@app/shared/utils';
 
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
-  styleUrls: ['./shared.component.scss']
+  styleUrls: ['./shared.component.scss'],
 })
-export class SharedComponent {
+export class SharedComponent implements OnInit {
+  form: FormGroup;
+  isInline: boolean;
+  regexErrors = regexErrors;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      input: [
+        null,
+        {
+          updateOn: 'blur',
+          validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern(regex.email),
+          ],
+        },
+      ],
+    });
+  }
+
+  onPatchValue() {
+    this.form.patchValue({ input: 'test' });
+  }
+
+  onToggleInline() {
+    this.isInline = !this.isInline;
+  }
+
+  onSubmit(): void {
+    console.log('Submit!!!');
+  }
 }
